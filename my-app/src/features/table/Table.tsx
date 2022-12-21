6dcc9ef0f6cf06e5a4f96a8cbf30d312b2f6fea4
LucaB <luca.bertoldi89@gmail.com>
2022-12-21 22:36:01 +0100

    add redux table pagination slice

---
+++ my-app/src/features/table/Table.tsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+
+import { useAppSelector, useAppDispatch } from "../../app/hooks";
+import { setPagination, selectPagination } from "./tableSlice";
+
+export const Table = () => {
+  const count = useAppSelector(selectPagination);
+
+  const dispatch = useAppDispatch();
+
+  return (
+    <div>
+      <h1>Comments List</h1>
+      <div
+        style={{
+          width: "200px",
+          height: "200px",
+          cursor: "pointer",
+          color: "red",
+          position: "relative",
+          display: "flex",
+        }}
+        onClick={() => {
+          dispatch(setPagination(5));
+        }}
+      >
+        ciao
+      </div>
+      {/* <table id="List">
+        <tr>
+          <th>Company</th>
+          <th>Contact</th>
+          <th>Country</th>
+        </tr>
+        <tr>
+          <td>Alfreds Futterkiste</td>
+          <td>Maria Anders</td>
+          <td>Germany</td>
+        </tr>
+        <tr>
+          <td>Berglunds snabbköp</td>
+          <td>Christina Berglund</td>
+          <td>Sweden</td>
+        </tr>
+        <tr>
+          <td>Centro comercial Moctezuma</td>
+          <td>Francisco Chang</td>
+          <td>Mexico</td>
+        </tr>
+        <tr>
+          <td>Ernst Handel</td>
+          <td>Roland Mendel</td>
+          <td>Austria</td>
+        </tr>
+        <tr>
+          <td>Island Trading</td>
+          <td>Helen Bennett</td>
+          <td>UK</td>
+        </tr>
+        <tr>
+          <td>Königlich Essen</td>
+          <td>Philip Cramer</td>
+          <td>Germany</td>
+        </tr>
+        <tr>
+          <td>Laughing Bacchus Winecellars</td>
+          <td>Yoshi Tannamuri</td>
+          <td>Canada</td>
+        </tr>
+        <tr>
+          <td>Magazzini Alimentari Riuniti</td>
+          <td>Giovanni Rovelli</td>
+          <td>Italy</td>
+        </tr>
+        <tr>
+          <td>North/South</td>
+          <td>Simon Crowther</td>
+          <td>UK</td>
+        </tr>
+        <tr>
+          <td>Paris spécialités</td>
+          <td>Marie Bertrand</td>
+          <td>France</td>
+        </tr>
+      </table> */}
+    </div>
+  );
+};
